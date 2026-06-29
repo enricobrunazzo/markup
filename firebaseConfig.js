6b749d1ea71abdeb9ba727fbfcec40015791a585
enricobrunazzo <89249309+enricobrunazzo@users.noreply.github.com>
2026-06-29 16:53:37 +0200

    fix: authDomain impostato a markup-pearl.vercel.app per risolvere redirect cross-origin

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,16 +1,8 @@
-// Import da CDN per window in ambiente browser
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js';
-
-// Definizione e inizializzazione Firebase direttamente in questo file
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyD8-Z-x9zEIZJpgemwgVOhELVURjzVDoWA",
-  authDomain: "markup-enr.firebaseapp.com",
+  authDomain: "markup-pearl.vercel.app",
   projectId: "markup-enr",
   storageBucket: "markup-enr.firebasestorage.app",
   messagingSenderId: "32509586403",
   appId: "1:32509586403:web:2de0bcc3ea2ce1a0194ed6"
 };
-
-const app = initializeApp(firebaseConfig);
-
-export { app, firebaseConfig };
